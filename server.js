c2e976e46f238570a867cd116f8ca13f9c10a5c6
Sanyogita <sanyogitap1705@gmai.com>
2025-07-17 12:35:44 +0530

    add api

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(express.json({ limit: '2mb' }));
 
 
 // --- 3. INITIALIZE GOOGLE AI CLIENT ---
-const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY; 
+const GOOGLE_API_KEY = "AIzaSyAoddfq6v-V1KxotfEFTW7KIomj8SoWcBg"; 
 const NETLIFY_ACCESS_TOKEN = process.env.NETLIFY_ACCESS_TOKEN;
 const NETLIFY_SITE_ID = process.env.NETLIFY_SITE_ID;
 if (!GOOGLE_API_KEY || !NETLIFY_ACCESS_TOKEN || !NETLIFY_SITE_ID) {
